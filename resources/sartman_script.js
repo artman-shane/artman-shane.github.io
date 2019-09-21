35cd2b108e0ccfb6e592812d6b0bb16038e04939
ShaneArtman <MrShaneArtman@Gmail.com>
2019-09-21 12:09:32 -0400

    Working on it...

--- resources/sartman_script.js
+++ resources/sartman_script.js
@@ -1,1 +1,4 @@
 /* This is the source of scripting for the sartman assignment portal */
+
+var lastmod = "Last Updated: " + document.lastModified;
+document.getElementById("lastupdate").innerHTML=lastmod;
