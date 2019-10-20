ddd99eb03c84801c78eff2507ba02ef01f463b1f
ShaneArtman <MrShaneArtman@Gmail.com>
2019-10-19 22:19:00 -0400

    Final Wk 5

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -1,32 +1,4 @@
-function toggleMenu() {
-    document.getElementsByClassName("nav-menu")[0].classList.toggle("displayed");
-}
-
-function toggleFullArticle() {
-    document.getElementsByClassName("full-article")[0].classList.toggle("displayed");
-    document.getElementsByClassName("disp-full-artcile")[0].classList.toggle("hide");
-}
 
 
-// Date format: Wednesday, 24 July 2020
-var todayDate = new Date()
-var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var todayDay = days[todayDate.getDay()];
-var todayNumDay = todayDate.getDate();
-var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var todayMonth = months[todayDate.getMonth()];
-var todayYear = todayDate.getFullYear();
-document.getElementById("footer-msg").innerHTML = todayDay + " ," + todayNumDay + " " + todayMonth + " " + todayYear;
 
 
-console.log("Today is: " + todayDay);
-if (todayDay == "Friday") {
-    document.getElementsByClassName("special-news")[0].classList.toggle("displayed");
-}
-
-var lastmod = "Last Updated: " + document.lastModified;
-document.getElementById("lastupdate").innerHTML=lastmod;
-
-let strDate = new Date();
-let year = strDate.getFullYear();
-document.getElementById("fullyear").innerHTML=year;
---
+++ lesson5/js/toggle-menu.js
@@ -0,0 +1,3 @@
+function toggleMenu() {
+    document.getElementsByClassName("nav-menu")[0].classList.toggle("displayed");
+}
