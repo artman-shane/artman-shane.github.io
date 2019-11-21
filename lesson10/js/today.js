9bc1fd82c457cb5cccc51fc9d3da195c7b154239
ShaneArtman <MrShaneArtman@Gmail.com>
2019-11-20 21:44:10 -0500

    Scoping

--- lesson10/js/today.js
+++ lesson10/js/today.js
@@ -1,10 +1,10 @@
 // Date format: Wednesday, 24 July 2020
 let updateTodayDate = new Date()
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-let todayDay = days[updateTodayDate.getDay()];
+let updateDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let updateMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let todayDay = updateDays[updateTodayDate.getDay()];
 let todayNumDay = updateTodayDate.getDate();
-let todayMonth = months[updateTodayDate.getMonth()];
+let todayMonth = updateMonths[updateTodayDate.getMonth()];
 let todayYear = updateTodayDate.getFullYear();
 document.getElementById("footer-msg").innerHTML = todayDay + " ," + todayNumDay + " " + todayMonth + " " + todayYear;
 
